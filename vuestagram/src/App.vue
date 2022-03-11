376219fec64728108957934b1f29e333727718ed
jy0813 <75197188+jy0813@users.noreply.github.com>
2022-03-11 16:36:59 +0900

    vuestagram

---
+++ vuestagram/src/App.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li v-if="step == 1" @click="step++">Next</li>
+      <li v-if="step == 2" @click="publish">발행</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container :post="post" :step="step" :image="image" @write="write = $event"/>
+  <button @click="moreBtn()" v-if="step == 0">더보기</button>
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input @change="upload" type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import Container from './components/Container.vue';
+import post from './instaData.js';
+import axios from 'axios';
+
+export default {
+  name: "App",
+  data() {
+    return {
+      post:post,
+      more:0,
+      step:0,
+      image:'',
+      write:'',
+    }
+  },
+  components: {
+    Container,
+  },
+  methods: {
+    moreBtn() {
+      axios.get(`https://codingapple1.github.io/vue/more${this.more}.json`)
+      .then((result) => {
+        this.post.push(result.data);
+        this.more++;
+      })
+    },
+    upload(e) {
+      let file = e.target.files
+      console.log(file[0]);
+      let url = URL.createObjectURL(file[0]);
+      console.log(url);
+      this.image = url;
+      this.step = 1;
+    },
+    publish() {
+      let board = {
+          name: "jae yun",
+          userImage: "https://placeimg.com/100/100/arch",
+          postImage: this.image,
+          likes: 36,
+          date: "May 15",
+          liked: false,
+          content: this.write,
+          filter: "perpetua"
+      };
+      this.post.unshift(board);
+      this.step = 0;
+    }
+  },
+};
+</script>
+
+<style>
+@import 'style.css';
+
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+  text-align: center;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ vuestagram/src/components/filterBox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div :class="k" class="filter-item" :style="{backgroundImage : `url(${image})`}">
+      <slot></slot>   
+    </div> 
+</template>
+
+<script>
+export default {
+  name:'filterbox',
+  props: {
+    image:String,
+    k:String,
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color : white;
+  background-size: cover;
+  background-position : center;
+}
+</style>
